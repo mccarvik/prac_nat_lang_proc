da70b96513a350d16b8cda63de8095c6c9683a95
unknown <mccarviks@gmail.com>
2023-05-14 22:00:07 -0400

    chunking

--- 2_nlp_pipeline.py
+++ 2_nlp_pipeline.py
@@ -1,7 +1,7 @@
 """
 chapter 2 - NLP Pipeline
 """
-
+import pdb
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -11,11 +11,20 @@ def run():
     """
     initial function
     """
+    bsdemo()
 
+
+def bsdemo():
     myurl = "https://stackoverflow.com/questions/415511/how-to-get-the-current-time-in-python"
     html = urlopen(myurl).read()
     soupified = BeautifulSoup(html, "html.parser")
-    questions = soupified.find_all("div", {"class": "question"})
+    question = soupified.find_all("div", {"class": "question"})[0]
+    questiontext = question.find("div", {"class": "s-prose js-post-body"})
+    print("Question: \n", questiontext.get_text().strip())
+    answer = soupified.find("div", {"class": "answer"})
+    pdb.set_trace()
+    answertext = answer.find("div", {"class": "s-prose js-post-body"})
+    print("Best answer: \n", answertext.get_text().strip())
 
 
 
